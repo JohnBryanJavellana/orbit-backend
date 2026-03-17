90714e566b2773d4074fc4952d851465f86e45a4
John The Bryan <johnbryanjavellana@gmail.com>
2026-03-17 16:18:42 +0800

    1123

--- app/Utils/Notifications.php
+++ app/Utils/Notifications.php
@@ -2,20 +2,18 @@
 
 namespace App\Utils;
 
-use App\Events\BENotification;
 use App\Models\Notification;
 
 class Notifications
 {
     public static function notify($userId, $to_user, $message){
-        return TransactionUtil::transact(null, [], function() use ($userId, $to_user, $message) {
-            $notif = new Notification();
-            $notif->user_id = $userId;
-            $notif->to_user = $to_user;
-            $notif->message = $message;
-            $notif->save();
+        $notif = new Notification();
+        $notif->from_user = $userId;
+        $notif->to_user = $to_user;
+        $notif->message = $message;
+        $notif->is_read = false;
+        $notif->save();
 
-            return response()->json(['message' => ""], 201);
-        });
+        return response()->json(['message' => ""], 201);
     }
 }
